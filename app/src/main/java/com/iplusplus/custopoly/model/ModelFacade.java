b156e746e4d75e6392116ab5aeac9dcd852df9a8
pegartillo95 <pegarc03@ucm.es>
2015-03-29 20:25:22 +0200

    Update ModelFacade.java

--- app/src/main/java/com/iplusplus/custopoly/model/ModelFacade.java
+++ app/src/main/java/com/iplusplus/custopoly/model/ModelFacade.java
@@ -1,11 +1,9 @@
 package com.iplusplus.custopoly.model;
-
-
+import com.iplusplus.custopoly.model.gamemodel.GameFacade;
 import com.iplusplus.custopoly.model.gamemodel.element.PropertyLand;
 import com.iplusplus.custopoly.model.gamemodel.element.Land;
-
+import java.util.ArrayList;
 import java.util.Collection;
-
 /**
  * SINGLETON class that has to:
  *              Provide an access point for the view.
@@ -17,9 +15,9 @@ import java.util.Collection;
  *                       }
  */
 public class ModelFacade {
-
     private static ModelFacade INSTANCE;
-
+    private static ShopKeeper shopKeeper;
+    private static ThemeHandler themeHandler;
     /**
      * Factory Method for Singleton object.
      *
@@ -31,69 +29,65 @@ public class ModelFacade {
     public static ModelFacade getInstance() {
         return INSTANCE;
     }
-
     //-------------
     // GAME methods
     //-------------
-
     /**
      * It instantiates all things necessary to begin a game.
      *
      * @param names
      *          A list containing the names of the players.
      */
-    public static void createGame(Collection<String> names) {}
-
+    public static void createGame(Collection<String> names) {
+        GameFacade.createGame(names);
+    }
     /**
      * Method that allows a player to purchase a property.
      *
      * @param land
      *          Cell in which the asset is located.
      */
-    public static void buyAsset(Land land){}
-
+    public static void buyAsset(Land land){
+        GameFacade.buyAsset(land);
+    }
     /**
      * Moves the player.
      *
      * @param playerName
      *          The name of the player that has to move.
      */
-    public static void move(String playerName) {}
-
+    public static void move(String playerName) {
+        GameFacade.move(playerName);
+    }
     /**
      * @param playerName
      *          The name of the player of which we want the assets.
      * @return
      *          Returns the asset list with the assets owned by.
      */
-    public static PropertyLand getAssetsOwnedByPlayer(String playerName) {
-        return null;
+    public static ArrayList<PropertyLand> getAssetsOwnedByPlayer(String playerName) {
+        return GameFacade.getAssetsOwnedByPlayer(playerName);
     }
-
-
-
     //-------------------
     // SHOPKEEPER methods
     //-------------------
-
     /**
      * Add some points to the player's wallet.
      *
      * @param points
      *              Number of points to add.
      */
-    public static void addPoints(int points) {}
-
+    public static void addPoints(int points) {
+        shopKeeper.addPoints(points);
+    }
     /**
      * Method to know how many points the owner has.
      *
      * @return
      *          Number of points in the wallet.
      */
-    public static int getPoints() {return 0;}
-
+    public static int getPoints() {return shopKeeper.getPoints();}
     //THEME HANDLING
-
     /**
      * Method to know if a given theme has already been purchased.
      *
@@ -103,8 +97,7 @@ public class ModelFacade {
      *              True if the theme has been purchased,
      *              False otherwise.
      */
-    public static boolean isThemePurchased(GameTheme theme) { return false;}
-
+    public static boolean isThemePurchased(GameTheme theme) { return shopKeeper.isThemePurchased(theme);}
     /**
      * Method to buy a theme.
      * It should also mark the corresponding player skins as purchased.
@@ -112,8 +105,7 @@ public class ModelFacade {
      * @param theme
      *              GameTheme to be bought.
      */
-    public static void buyTheme(GameTheme theme) {}
-
+    public static void buyTheme(GameTheme theme) {shopKeeper.buyTheme(theme);}
     /**
      * Method to know how much a theme costs
      *
@@ -122,10 +114,8 @@ public class ModelFacade {
      * @return
      *              The number of points it costs.
      */
-    public static int getThemeCost(GameTheme theme) {return 0;}
-
+    public static double getThemeCost(GameTheme theme) {return shopKeeper.getThemeCost(theme.getName());}
     //PLAYER SKIN HANDLING
-
     /**
      * Method to know if a certain player skin has been purchased.
      *
@@ -135,16 +125,14 @@ public class ModelFacade {
      *              True if the skin has been purchased,
      *              False otherwise.
      */
-    public static boolean isPlayerSkinPurchased (PlayerSkin skin) { return false;}
-
+    public static boolean isPlayerSkinPurchased (PlayerSkin skin) { return shopKeeper.isPlayerSkinPurchased(skin);}
     /**
      * Method to buy a certain player skin.
      *
      * @param skin
      *              Skin to be bought.
      */
-    public static void buyPlayerSkin(PlayerSkin skin) {}
-
+    public static void buyPlayerSkin(PlayerSkin skin) {shopKeeper.buyPlayerSkin(skin);}
     /**
      * Method to know how much a certain skin costs.
      *
@@ -153,20 +141,17 @@ public class ModelFacade {
      * @return
      *              The price of the skin.
      */
-    public static int getPlayerSkinCost (PlayerSkin skin) {return 0;}
-
+    public static double getPlayerSkinCost (PlayerSkin skin) {return shopKeeper.getPlayerSkinCost(skin.getName());}
     //---------------------
     // THEMEHANDLER methods
     //---------------------
-
     /**
      * Method to change the current theme.
      *
      * @param theme
      *              The theme it should change to.
      */
-    public static void switchThemeTo(GameTheme theme) {}
-
+    public static void switchThemeTo(GameTheme theme) {themeHandler.switchThemeTo(theme);}
     /**
      * Method to change the skin on a particular player.
      *
@@ -175,8 +160,7 @@ public class ModelFacade {
      * @param playerName
      *              Name of the player whose skin we want changed.
      */
-    public static void switchPlayerSkinTo (PlayerSkin skin, String playerName) { }
-
+    public static void switchPlayerSkinTo (PlayerSkin skin, String playerName) {themeHandler.switchPlayerSkinTo(skin,playerName); }
     /**
      * Method to retrieve a specific player's skin's resource uri.
      *
@@ -186,9 +170,8 @@ public class ModelFacade {
      *              Resource uri to access the image.
      */
     public static String getPlayerSkinUri(String playerName) {
-        return null;
+        return themeHandler.getPlayerSkinUri(playerName);
     }
-
     /**
      * Method to retrieve the link to the current theme's board graphic.
      *
@@ -196,9 +179,8 @@ public class ModelFacade {
      *          The link to the current theme's board graphic asset.
      */
     public static String getBoardGraphicUri() {
-        return null;
+        return themeHandler.getBoardGraphicUri();
     }
-
     /**
      * Returns the theme currently being used.
      *
@@ -206,9 +188,8 @@ public class ModelFacade {
      *          The theme currently on use.
      */
     public static GameTheme getCurrentTheme() {
-        return null;
+        return themeHandler.getCurrentTheme();
     }
-
     /**
      * Returns the player skin for the desired player.
      *
@@ -217,5 +198,5 @@ public class ModelFacade {
      * @return
      *              The skin of the player.
      */
-    public static PlayerSkin getPlayerSkin (String playerName) {return null;}
+    public static PlayerSkin getPlayerSkin (String playerName) {return themeHandler.getPlayerSkin(playerName);}
 }
