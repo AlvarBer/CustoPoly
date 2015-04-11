35a52432ab5b2e5281723b8f022c6dc6cf52ff9a
Mortadelegle <mortadelegle@gmail.com>
2015-04-11 12:25:37 +0200

    Fixed shop being enabled & AppTheme is getting bigger

--- app/src/main/java/com/iplusplus/custopoly/app/InitActivity.java
+++ app/src/main/java/com/iplusplus/custopoly/app/InitActivity.java
@@ -3,21 +3,12 @@ package com.iplusplus.custopoly.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import com.iplusplus.custopoly.model.GameTheme;
 import com.iplusplus.custopoly.model.PlayerSkin;
 import com.iplusplus.custopoly.model.ThemeHandler;
-import com.iplusplus.custopoly.model.gamemodel.command.AskBuyCommand;
-import com.iplusplus.custopoly.model.gamemodel.command.Command;
-import com.iplusplus.custopoly.model.gamemodel.element.Board;
-import com.iplusplus.custopoly.model.gamemodel.element.Game;
-import com.iplusplus.custopoly.model.gamemodel.element.Player;
-import com.iplusplus.custopoly.model.gamemodel.util.BoardFactory;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -57,27 +48,5 @@ public class InitActivity extends ActionBarActivity {
         Intent act = new Intent(this, MainActivity.class);
         startActivity(act);
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_init, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
