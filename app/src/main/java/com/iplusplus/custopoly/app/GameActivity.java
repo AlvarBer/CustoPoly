9807aec2d2be583ea9069ed501b899cbf52bd954
Fran Lozano <franloza@ucm.es>
2015-04-19 21:26:15 +0200

    Skins are place on the board of GameActivity

--- app/src/main/java/com/iplusplus/custopoly/app/GameActivity.java
+++ app/src/main/java/com/iplusplus/custopoly/app/GameActivity.java
@@ -2,12 +2,17 @@ package com.iplusplus.custopoly.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import com.iplusplus.custopoly.model.GameTheme;
 import com.iplusplus.custopoly.model.SaveGameHandler;
+import com.iplusplus.custopoly.model.Utilities;
 import com.iplusplus.custopoly.model.gamemodel.element.Game;
+import com.iplusplus.custopoly.model.gamemodel.element.Player;
 
 import java.io.IOException;
 
@@ -26,6 +31,10 @@ public class GameActivity extends ActionBarActivity {
     //Attributes
     private Game game;
     private ImageView boardBackground;
+    private FrameLayout players;
+
+    //Constants
+    private static final int SPACE_WIDTH = 390;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +52,8 @@ public class GameActivity extends ActionBarActivity {
         }
 
         setupViews();
-        buildBoard();
+        drawBoard();
+        drawPlayers();
 
     }
 
@@ -70,11 +80,59 @@ public class GameActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void buildBoard() {
+    private void drawPlayers() {
+        LayoutInflater inflater = getLayoutInflater();
+        int i = 0;
+        for (Player player : game.getPlayers()) {
+            View view = inflater.inflate(R.layout.player, players,false);
+            ImageView skin = (ImageView) view.findViewById(R.id.player_iv_skin);
+            skin.setImageResource(player.getSkin().getImageResourceId());
+            view.setTag(i);
+            view.setX(Utilities.dpToPx(
+                    (int) (SPACE_WIDTH * (player.getLandIndex() + 0.5)), this)
+                    + calculateSpaceRelativePositionX(player));
+            view.setY(calculatePositionY(player));
+            players.addView(view);
+
+            i++;
+        }
+    }
+
+    private void drawBoard() {
         this.boardBackground.setImageResource(game.getTheme().getBackgroundPathResource());
     }
 
     private void setupViews() {
         this.boardBackground = (ImageView) findViewById(R.id.activity_game_iv_boardBackground);
+        this.players = (FrameLayout)findViewById(R.id.activity_game_fl_players);
     }
+
+    /**
+     * Utility function for all functions related to moving the player views.
+     * Calculates the X position of a player relative to the center of his
+     * current space, so that the players don't all sit on the same spot.
+     *
+     * @param player
+     *            the player to calculate the position for
+     * @return X position relative to the center of the current space, in pixels
+     */
+    private int calculateSpaceRelativePositionX(Player player) {
+        return Utilities.dpToPx((player.getPlayerID() % 2 == 0 ? 1 : -1) * 12,
+                this);
+    }
+
+    /**
+     * Utility function for all functions related to moving the player views.
+     * Calculates the Y position of a player, so that the players don't all sit
+     * on the same spot.
+     *
+     * @param player
+     *            the player to calculate the position for
+     * @return Y position in pixels
+     */
+    private int calculatePositionY(Player player) {
+        return Utilities.dpToPx(146 + 22 * (player.getPlayerID() / 2), this);
+    }
+
+
 }
