3704a37595551ca606f127efcceb43ac588043ae
Mortadelegle <mortadelegle@gmail.com>
2015-04-21 13:09:13 +0200

    Removed little bar on the up & settings prompt
    Archieved this by removing the two extra functions no one wasu sing  and also adding those two magic lines on Oncreate

--- app/src/main/java/com/iplusplus/custopoly/app/ShopActivity.java
+++ app/src/main/java/com/iplusplus/custopoly/app/ShopActivity.java
@@ -1,9 +1,9 @@
 package com.iplusplus.custopoly.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Corresponds with the shop_activity in the mockup.
@@ -19,30 +19,11 @@ public class ShopActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //These will put the app on full screen
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_shop, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
