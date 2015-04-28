92f90d8e566ab165ef803119c10e4b88c4cda1f9
blorente <blorente@ucm.es>
2015-04-29 00:32:15 +0200

    Fixed load and save (for now)
    Started implementing counter drawing methods.

--- app/src/main/java/com/iplusplus/custopoly/model/SaveGameHandler.java
+++ app/src/main/java/com/iplusplus/custopoly/model/SaveGameHandler.java
@@ -1,5 +1,6 @@
 package com.iplusplus.custopoly.model;
 
+import java.io.IOException;
 import android.content.Context;
 import com.iplusplus.custopoly.Custopoly;
 import com.iplusplus.custopoly.model.gamemodel.GameFacade;
@@ -19,27 +20,27 @@ public class SaveGameHandler {
 
     private String GENERIC_GAME_NAME = Custopoly.getAppContext().getString(R.string.generic_game_name);
 
-        private static SaveGameHandler INSTANCE;
-        /**
-         * Constructor of the class. Private as it's a Singleton class
-         */
-        private SaveGameHandler() {}
+    private static SaveGameHandler INSTANCE;
 
-        /**
-         * Factory Method for Singleton object.
-         *
-         * @return
-         *          Returns the single instance of SaveGameHandler that there should be.
-         *          It initializes INSTANCE if it's not initialized, and then return it.
-         */
-        public static SaveGameHandler getInstance() {
-            if (INSTANCE == null)
-            {
-                // Create the instance
-                INSTANCE = new SaveGameHandler();
-            }
-            return INSTANCE;
+    /**
+     * Constructor of the class. Private as it's a Singleton class
+     */
+    private SaveGameHandler() {
+    }
+
+    /**
+     * Factory Method for Singleton object.
+     *
+     * @return Returns the single instance of SaveGameHandler that there should be.
+     * It initializes INSTANCE if it's not initialized, and then return it.
+     */
+    public static SaveGameHandler getInstance() {
+        if (INSTANCE == null) {
+            // Create the instance
+            INSTANCE = new SaveGameHandler();
         }
+        return INSTANCE;
+    }
 
     /**
      * Save a Custopoly game to a file.
@@ -112,7 +113,7 @@ public class SaveGameHandler {
             FileInputStream fis;
             fis = context.openFileInput(this.GENERIC_GAME_NAME);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            GameFacade game = (Game) ois.readObject();
+            Game game = (Game) ois.readObject();
             ois.close();
             fis.close();
             return game;
--- app/src/main/java/com/iplusplus/custopoly/model/gamemodel/behaviour/ConstructionAllowance.java
+++ app/src/main/java/com/iplusplus/custopoly/model/gamemodel/behaviour/ConstructionAllowance.java
@@ -1,8 +1,10 @@
 package com.iplusplus.custopoly.model.gamemodel.behaviour;
 
-public interface ConstructionAllowance {
-	
-	public static final ConstructionAllowance CONSTRUCTION_ALLOWED = new ConstructAllowed();
+import java.io.Serializable;
+
+public interface ConstructionAllowance extends Serializable {
+
+    public static final ConstructionAllowance CONSTRUCTION_ALLOWED = new ConstructAllowed();
 	public static final ConstructionAllowance CONSTRUCTION_DENIED = new ConstructDenied();
 	
 	public abstract boolean isConstructionAllowed();
