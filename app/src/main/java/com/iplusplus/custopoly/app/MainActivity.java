c014a22f653b255fe4bb5d8247cfb1c366583885
Danireyes <dreyes01@ucm.es>
2015-05-07 00:04:52 +0200

    silly error

--- app/src/main/java/com/iplusplus/custopoly/app/MainActivity.java
+++ app/src/main/java/com/iplusplus/custopoly/app/MainActivity.java
@@ -65,4 +65,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 break;
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
