2eb8630c344d623cee8efe69d47d419ba10266d2
blorente <blorente@ucm.es>
2015-05-27 00:29:35 +0200

    Fixed first version of shop.

--- app/src/main/java/com/iplusplus/custopoly/app/ShopActivity.java
+++ app/src/main/java/com/iplusplus/custopoly/app/ShopActivity.java
@@ -1,12 +1,20 @@
 package com.iplusplus.custopoly.app;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -29,19 +37,21 @@ import java.util.HashMap;
 
 public class ShopActivity extends Activity implements  View.OnClickListener {
 
-        private LinearLayout themesLayout;
-        private TextView playerName;
-        private ImageView selectedView;
-        private TextView playerMoney;
-        private ShopKeeper shopKeeper = ThemeHandler.getInstance().getShopKeeperInstance();
-        private GameTheme selectedTheme = null;
-        private HashMap<ImageView, GameTheme> themeDict = new HashMap<ImageView, GameTheme>();
+    private LinearLayout themesLayout;
+    private TextView playerName;
+    private ImageView selectedView;
+    private TextView playerMoney;
+    private ShopKeeper shopKeeper = ThemeHandler.getInstance().getShopKeeperInstance();
+    private GameTheme selectedTheme = null;
+    private Handler handler;
+    private HashMap<ImageView, GameTheme> themeDict = new HashMap<ImageView, GameTheme>();
 
-        private boolean buy = false;
+    private boolean buy = false;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
@@ -50,120 +60,130 @@ public class ShopActivity extends Activity implements  View.OnClickListener {
 
         setupViews();
         fillWithContent();
+
+    }
+    private void setupViews()
+    {
+            this.themesLayout = (LinearLayout) findViewById(R.id.activity_shop_ll_themes);
+            //this.playerName = (TextView) findViewById(R.id.activity_shop_tv_player);
+            this.playerMoney = (TextView) findViewById(R.id.activity_shop_tv_money);
+            this.playerMoney.setText(this.playerMoney.getText() + "i$$ "
+                    + ThemeHandler.getInstance().getShopKeeperInstance().getPoints());
+    }
+
+    private void fillWithContent(){
+        this.themesLayout.removeAllViews();
+        for (GameTheme theme : this.shopKeeper.getThemesInShopList()) {
+            GridLayout layout;
+            layout = createThemeView(theme);
+
+            this.themesLayout.addView(layout);
         }
-        private void setupViews()
-                {
-                this.themesLayout = (LinearLayout) findViewById(R.id.activity_shop_ll_themes);
-                //this.playerName = (TextView) findViewById(R.id.activity_shop_tv_player);
-                this.playerMoney = (TextView) findViewById(R.id.activity_shop_tv_money);
-                this.playerMoney.setText(this.playerMoney.getText() + "i$$ "
-                + ThemeHandler.getInstance().getShopKeeperInstance().getPoints());
-                }
+    }
 
-        private void fillWithContent()
-                {
-                for (GameTheme theme : this.shopKeeper.getThemesInShopList()) {
-                        LinearLayout layout;
-                        layout = createThemeView(theme);
+    private GridLayout createThemeView(GameTheme theme) {
+        GridLayout layout = new GridLayout(this);
+        layout.setColumnCount(1);
+        layout.setRowCount(3);
 
-                        this.themesLayout.addView(layout);
-                }
-                }
-
-        private LinearLayout createThemeView(GameTheme theme) {
-                LinearLayout layout = new LinearLayout(Custopoly.getAppContext());
-                layout.setOrientation(LinearLayout.VERTICAL);
-
-                //Set up image
-                ImageView themeImage = new ImageView(Custopoly.getAppContext());
-                themeImage.setBackgroundColor(Color.BLUE);
-                themeImage.setMinimumHeight(50);
-                themeImage.setMinimumWidth(50);
-                //themeImage.setImageResource(getResources().getIdentifier(theme.getBackgroundPathResource(), "drawable", getPackageName()));
-                this.themeDict.put(themeImage, theme);
-                if (!this.shopKeeper.isThemePurchased(theme)) {
+        //Set up image
+        ImageView themeImage = new ImageView(this);
+        themeImage.setImageResource(getResources().getIdentifier(theme.getBackgroundPathResource(), "drawable", getPackageName()));
+        this.themeDict.put(themeImage, theme);
+        if (!this.shopKeeper.isThemePurchased(theme)) {
                 themeImage.setOnClickListener(this);
-                } else {
+        } else {
                 themeImage.setEnabled(false);
-                }
-
-                //Set up name
-                TextView themeName = new TextView(Custopoly.getAppContext());
-                themeName.setText(theme.getName());
-                themeName.setTextSize(30.0f);
-
-                //Set up cost
-                TextView themeCost = new TextView(Custopoly.getAppContext());
-                themeCost.setText("i$$ " + Double.toString(theme.getPrice()));
-                themeCost.setTextSize(30.0F);
-                ShopActivity.this.finish();
-                //Add everything to the layout
-                layout.addView(themeImage);
-                layout.addView(themeName);
-                layout.addView(themeCost);
-
-                return layout;
+                themeImage.setAlpha(0.5f);
         }
 
-        @Override
-        //TODO: THIS WILL BREAK IF WE ARE LISTENING TO MORE THAN THE THEME IMAGES
-        public void onClick(View view) {
-            if (view instanceof ImageView) {
-                this.selectedTheme = this.themeDict.get(view);
-                this.selectedView = (ImageView) view;
-                if (this.selectedTheme != null) {
-                    if (this.shopKeeper.getPoints() >= this.selectedTheme.getPrice()) {
-                    //Show wanna buy dialog
-                        showWantToBuyDialog();
-                    } else {
-                    //Show not enough points dialog
-                        showNotEnoughPointsDialog();
-                    }
+        themeImage.setLayoutParams(this.createGridLayoutParams(0, 0));
+
+        //Set up name
+        TextView themeName = new TextView(this);
+        themeName.setText(theme.getName());
+        themeName.setTextSize(30.0f);
+        themeName.setLayoutParams(this.createGridLayoutParams(0, 1));
+
+        //Set up cost
+        TextView themeCost = new TextView(this);
+        themeCost.setText("i$$ " + Double.toString(theme.getPrice()));
+        themeCost.setTextSize(30.0F);
+        themeCost.setLayoutParams(this.createGridLayoutParams(0, 2));
+
+        //Add everything to the layout
+        layout.addView(themeImage);
+        layout.addView(themeName);
+        layout.addView(themeCost);
+
+        //layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+        //                                                 ViewGroup.LayoutParams.MATCH_PARENT));
+
+        return layout;
+    }
+
+    private ViewGroup.LayoutParams createGridLayoutParams(int col, int row) {
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+        params.columnSpec = GridLayout.spec(col);
+        params.rowSpec = GridLayout.spec(row);
+        return params;
+    }
+
+    @Override
+    //TODO: THIS WILL BREAK IF WE ARE LISTENING TO MORE THAN THE THEME IMAGES
+    public void onClick(View view) {
+        if (view instanceof ImageView) {
+            this.selectedTheme = this.themeDict.get(view);
+            this.selectedView = (ImageView) view;
+            if (this.selectedTheme != null) {
+                if (this.shopKeeper.getPoints() >= this.selectedTheme.getPrice()) {
+                //Show wanna buy dialog
+                    showWantToBuyDialog();
+                } else {
+                //Show not enough points dialog
+                    showNotEnoughPointsDialog();
                 }
             }
         }
+    }
 
-        private void showWantToBuyDialog() {
-                new AlertDialog.Builder(this)
-                .setMessage(Custopoly.getAppContext().getString(R.string.shop_wanttobuy_message))
-                .setTitle(Custopoly.getAppContext().getString(R.string.shop_wanttobuy_title))
-                .setPositiveButton(Custopoly.getAppContext().getString(R.string.shop_yes),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                            buyProperty();
-                            }
-
-                            })
-                .setNegativeButton(Custopoly.getAppContext().getString(R.string.shop_no), new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-                }
-                })
+    private void showWantToBuyDialog() {
+        new AlertDialog.Builder(this)
+        .setMessage(Custopoly.getAppContext().getString(R.string.shop_wanttobuy_message))
+        .setTitle(Custopoly.getAppContext().getString(R.string.shop_wanttobuy_title))
+        .setPositiveButton(Custopoly.getAppContext().getString(R.string.shop_yes),  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    buyProperty(); } })
+        .setNegativeButton(Custopoly.getAppContext().getString(R.string.shop_no), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            } })
                 .show();
 
-                }
+    }
 
-        private void buyProperty() {
-                this.shopKeeper.buyTheme(this.selectedTheme);
-                new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setMessage(String.format(Custopoly.getAppContext().getString(R.string.shop_themebought_message), this.selectedTheme.getName()))
-                .setTitle(Custopoly.getAppContext().getString(R.string.shop_themebought_title))
-                .show();
-                this.playerMoney.setText("MONEY: i$$" + ThemeHandler.getInstance().getShopKeeperInstance().getPoints());
-                this.selectedView.setEnabled(false);
-                this.selectedView = null;
-                this.selectedTheme = null;
-        }
+    private void buyProperty() {
+        this.shopKeeper.buyTheme(this.selectedTheme);
+        new AlertDialog.Builder(this)
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        .setMessage(String.format(Custopoly.getAppContext().getString(R.string.shop_themebought_message), this.selectedTheme.getName()))
+        .setTitle(Custopoly.getAppContext().getString(R.string.shop_themebought_title))
+        .show();
+        this.playerMoney.setText("MONEY: i$$" + ThemeHandler.getInstance().getShopKeeperInstance().getPoints());
+        this.selectedView.setEnabled(false);
+        this.selectedView.setAlpha(0.5f);
+        this.selectedView = null;
+        this.selectedTheme = null;
+    }
 
-        private void showNotEnoughPointsDialog() {
-                new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setMessage(Custopoly.getAppContext().getString(R.string.shop_notenoughfunds_message))
-                .setTitle(Custopoly.getAppContext().getString(R.string.shop_notenoughfunds_title))
-                .show();
-        }
+    private void showNotEnoughPointsDialog() {
+        new AlertDialog.Builder(this)
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        .setMessage(Custopoly.getAppContext().getString(R.string.shop_notenoughfunds_message))
+        .setTitle(Custopoly.getAppContext().getString(R.string.shop_notenoughfunds_title))
+        .show();
+    }
 
     @Override
     public void onBackPressed() {
@@ -171,4 +191,5 @@ public class ShopActivity extends Activity implements  View.OnClickListener {
         ShopActivity.this.finish();
         startActivity(play);
     }
+
 }
